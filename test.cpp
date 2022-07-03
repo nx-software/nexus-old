82bad43807796af8ed2919129a41add4b9324b80
Electro-Corp <chinmaytanmay@outlook.com>
2022-07-03 16:35:06 -0600

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,24 @@
+#include "mainengine.h"
+int main(){
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f, // left  
+         0.5f, -0.5f, 0.0f, // right 
+         0.0f,  0.5f, 0.0f  // top   
+	}; 
+    const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+    const char *fragShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+    std::list<object> objects;
+    object real(vertices,vertexShaderSource,fragShaderSource);
+    objects.push_back(real);
+    mainLoop("Game Test",600,800,objects);
+}
