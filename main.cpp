1155395968dc8ec4c2b0a2c60e616881404ff5e8
Electro-Corp <chinmaytanmay@outlook.com>
2022-07-03 10:25:12 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,37 +1,30 @@
 /*
 3D Engine v0.0.1
 By:
-Chinmay 
-Lucas
+Chinmay Tiwari
 (add other contribuiters here)
 */
 #include <iostream>
 #include "glad/src/glad.c"
 #include "glad/include/glad/glad.h"
-#include <GL/gl.h>
+//#include <GL/gl.h>
 #include <GLFW/glfw3.h>
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 //testing models and inputs
-float vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-     0.0f,  0.5f, 0.0f
-}; 
-const char *vertexShaderSource = 
-"#version 330 core\n"
-"layout (location = 0) in ve3 aPos;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
-const char *fragShaderSource = 
-"#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-"}\0";
+
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+const char *fragShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
 int main(){
 	std::cout<<"3D Engine\n";
 	/*
@@ -56,11 +49,6 @@ int main(){
 	}  
 	//OpenGL viewport
 	glViewport(0,0,800,600);
-	//render buffer stuff
-	unsigned int VBO;
-	glGenBuffers(1,&VBO);
-	glBindBuffer(GL_ARRAY_BUFFER,VBO);
-	glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices, GL_STATIC_DRAW);
 	//shader
 	unsigned int vertexShader;
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -83,27 +71,36 @@ int main(){
 	//link vertex attribuites
 	glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(float),(void*)0);
 	glEnableVertexAttribArray(0);
-	//Generate VAO
-	unsigned int VAO;
-	glGenVertexArrays(1,&VAO);
-	glBindVertexArray(VAO);
-	glBindBuffer(GL_ARRAY_BUFFER,VBO);
-	glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
-	glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(float),(void*)0);
-	glEnableVertexAttribArray(0);
+
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f,  0.5f, 0.0f
+	}; 
+
+	unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+    glBindVertexArray(0); 
 	//main loop
 	while(!glfwWindowShouldClose(window)){
-		processInput(window);
-		//Renderr
-		glClearColor(0.2f,0.3f,0.9f,1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-		//draw triangle
-		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES,0,3);
-		//glfwStuff
-		glfwSwapBuffers(window);
-		glfwPollEvents();
+        processInput(window);
+
+        glClearColor(0.1f, 0.4f, 0.9f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO); 
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
 	}
 	glfwTerminate();
 	return 0;
